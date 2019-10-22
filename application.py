e27771257f9a4f6026cc4165ee91a40ae599eb7c
khanmaazmo <55481140+khanmaazmo@users.noreply.github.com>
2019-10-22 09:08:21 +0400

    Add files via upload

--- application.py
+++ application.py
@@ -1,43 +1,47 @@
 from flask import Flask
 from flask import url_for
 from flask_restplus import Api, Resource, fields
-#from werkzeug.contrib.fixers import ProxyFix #added
 
-#option 2 not recommended
-#from werkzeug.contrib.fixers import ProxyFix
-#app = Flask(__name__)
-#app.wsgi_app = ProxyFix(app.wsgi_app)
-#OR
-#from werkzeug.middleware.proxy_fix import ProxyFix
-#app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+# from werkzeug.contrib.fixers import ProxyFix #added
 
-#option 1
+# option 2 not recommended
+# from werkzeug.contrib.fixers import ProxyFix
+# app = Flask(__name__)
+# app.wsgi_app = ProxyFix(app.wsgi_app)
+# OR
+# from werkzeug.middleware.proxy_fix import ProxyFix
+# app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+# option 1
+app = Flask(__name__)
+
+
 @property
 def specs_url(self):
-   #"""Monkey patch for HTTPS"""
-   return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+    # """Monkey patch for HTTPS"""
+    return url_for(self.endpoint('specs'), _external=True, _scheme='https')
+
 
 Api.specs_url = specs_url
 
-app = Flask(__name__)
-#app.wsgi_app = ProxyFix(app.wsgi_app)  #added
-api = Api(app) #, title='TodoMVC API', description='A simple TodoMVC API'
-#name_space = app.namespace('main', description='Main APIs')
+# app.wsgi_app = ProxyFix(app.wsgi_app)  #added
+api = Api(app)  # , title='TodoMVC API', description='A simple TodoMVC API'
+# name_space = app.namespace('main', description='Main APIs')
 
 a_language = api.model('Language', {'language': fields.String('The Language.')})
 a_language2 = api.model('Languagesss', {'languagesss': fields.String('The Languagessss.')})
 
-#todo = api.model('Todo', {
+# todo = api.model('Todo', {
 #    'id': fields.Integer(readonly=True, description='The task unique identifier'),
 #    'task': fields.String(required=True, description='The task details')
-#})
+# })
 
 languages = []
 python = {'language': 'Python'}
 languages.append(python)
 
 
-@api.route('/language') #@ns.route('/language') if namespace is used
+@api.route('/language')  # @ns.route('/language') if namespace is used
 class Language(Resource):
     @api.expect(a_language2)
     def get(self):
